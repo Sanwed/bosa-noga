214391188f178340b4f788e97205316f697deb24
Александр Козюков <sanwed@Noutbuk-Aleksandr.local>
2025-05-03 13:23:18 +0400

    Правки

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename="/bosa-noga/">
+    <HashRouter>
       <Provider store={store}>
         <HelmetProvider>
           <App />
